b45444fc3040c359bf33cced6bb609185ca17cc5
BuildTools <juelzlax@gmail.com>
2024-08-04 00:43:38 -0400

    Created a static, un-styled version of the home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View, FlatList, Image, Button } from "react-native";
 import React, { useEffect, useState } from "react";
+import ScanCell from "../components/ScanCell";
 
 export default function HomeScreen() {
-  const data = { name: "John", age: 30, car: null };
+  const [scan, setScan] = useState([
+    { key: "1", name: "Sunscreen", brand: "Dove", time: "12 hours" },
+    { key: "2", name: "Cleanser", brand: "Cerave", time: "7 months" }
+  ]);
 
   return (
     <View style={styles.container}>
@@ -12,30 +16,7 @@ export default function HomeScreen() {
       </View>
 
       {/* scanList */}
-      <FlatList
-        style={styles.scanList}
-        data={data}
-        renderItem={({ item }) =>
-          <View style={styles.scanCell}>
-            <Image style={styles.scanThumbnail} source={{ uri: `https://www.coinlore.com/img/${item.nameid}.png` }} />
-            <View style={styles.scanName}>
-              <Text style={styles.scanNameText}>
-                {item.name}
-              </Text>
-            </View>
-            <View style={styles.scanBrand}>
-              <Text style={styles.scanBrandText}>
-                {item.brand}
-              </Text>
-            </View>
-            <View style={styles.scanTime}>
-              <Text style={styles.scanTimeText}>
-                {item.time}
-              </Text>
-            </View>
-          </View>}
-        keyExtractor={item => item.id}
-      />
+      <FlatList style={styles.scanList} data={scan} renderItem={({ item }) => <ScanCell name={item.name} brand={item.brand} time={item.time} />} />
 
       {/* scanButton */}
       <Button title="Scan" />
@@ -46,8 +27,19 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    display: "flex",
+    backgroundColor: "#FAF9F6",
+    paddingVertical: 60,
+    paddingHorizontal: 20
+  },
+  header: {
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd"
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  scanList: {}
 });
